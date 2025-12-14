e79677aa0977599c3ce727245af039d0a12a2f49
thomas bourrely <thomas.bourrely@protonmail.com>
2025-12-14 15:20:43 +0100

    chore(backend): enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: true, credentials: true });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
