fbf01c248cbf5f3a6579209eff809600b363d398
thomas bourrely <thomas.bourrely@protonmail.com>
2025-12-18 11:16:29 +0100

    chore(frontend): create svg files for actions menu icons

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
       target: "react",
       autoCodeSplitting: true,
     }),
+    svgr(),
     react(),
   ],
 });
