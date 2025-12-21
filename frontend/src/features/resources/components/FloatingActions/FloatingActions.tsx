1a7c7c20575ec9ef6466e7e12445456d85040e0d
thomas bourrely <thomas.bourrely@protonmail.com>
2025-12-21 18:22:59 +0100

    feat(frontent/resource view): hide floating actions by default

--- frontend/src/features/resources/components/FloatingActions/FloatingActions.tsx
+++ frontend/src/features/resources/components/FloatingActions/FloatingActions.tsx
@@ -2,6 +2,7 @@ import Check from './icons/check.svg?react';
 import Eye from './icons/eye.svg?react';
 import Bin from './icons/bin.svg?react';
 import Fullscreen from './icons/fullscreen.svg?react';
+import { useState } from 'react';
 
 type FloatingActionsProps = {
   read: boolean;
@@ -11,35 +12,49 @@ type FloatingActionsProps = {
 };
 
 export default function FloatingActions({ read, onToggleRead, onDelete, onToggleFullScreen }: FloatingActionsProps) {
+
+  const [hidden, setHidden] = useState(true);
+
   return (
-    <div className="fixed right-6 top-1/2 z-50 flex flex-col gap-2">
+    <>
+      <div className={`fixed bottom-14 px-2 rounded z-50 ${hidden ? '-left-full' : 'left-5'} transition-all`}>
+        <div className="flex gap-2">
+          {onToggleFullScreen && (
+            <button
+              onClick={onToggleFullScreen}
+              className="bg-white border p-2 rounded shadow hover:bg-gray-50"
+              title="Toggle Fullscreen"
+            >
+              <Fullscreen />
+            </button>
+          )}
 
-      {onToggleFullScreen && (
-        <button
-          onClick={onToggleFullScreen}
-          className="bg-white border p-2 rounded shadow hover:bg-gray-50"
-          title="Toggle Fullscreen"
-        >
-          <Fullscreen />
-        </button>
-      )}
+          <button
+            onClick={onToggleRead}
+            className="bg-white border p-2 rounded shadow hover:bg-gray-50"
+            aria-pressed={read}
+            title={read ? 'Mark as unread' : 'Mark as read'}
+          >
+            {read ? <Check /> : <Eye />}
+          </button>
+
+          <button
+            onClick={onDelete}
+            className="bg-white border p-2 rounded shadow text-red-600 hover:bg-gray-50"
+            title="Delete"
+          >
+            <Bin />
+          </button>
+        </div>
+      </div>
 
       <button
-        onClick={onToggleRead}
-        className="bg-white border p-2 rounded shadow hover:bg-gray-50"
-        aria-pressed={read}
-        title={read ? 'Mark as unread' : 'Mark as read'}
+        onClick={() => setHidden(!hidden)}
+        className="fixed z-55 left-0 bottom-15 p-1 shadow hover:bg-gray-100 hover:cursor-pointer"
+        title={hidden ? 'Show actions' : 'Hide actions'}
       >
-        {read ? <Check /> : <Eye />}
+        {hidden ? '»' : '«'}
       </button>
-
-      <button
-        onClick={onDelete}
-        className="bg-white border p-2 rounded shadow text-red-600 hover:bg-gray-50"
-        title="Delete"
-      >
-        <Bin />
-      </button>
-    </div>
+    </>
   )
 }
